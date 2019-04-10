9681c6ab7fcfeed121a82f915bad0d9a76f1ac3c
Mathieu Boudreau <emb6150@gmail.com>
2019-04-10 17:14:07 -0230

    Initial Octave example

--- testing_your_code/math/substract.py
+++
@@ -1,8 +0,0 @@
-
-
-def substract(a, b):
-    """ Substract two mathematical objects together
-    :return: substraction of b from a
-    """
-
-    return a - b
--- tests/testing_your_code/math/add_Test.m
+++ tests/testing_your_code/math/add_Test.m
@@ -1,34 +1,25 @@
-classdef (TestTags = { 'unit'}) add_Test < matlab.unittest.TestCase
-
-    properties
+function test_suite=add_Test
+    try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+        test_functions=localfunctions();
+    catch % no problem; early Matlab versions can use initTestSuite fine
     end
+    initTestSuite;
 
-    methods (TestClassSetup)
-    end
+% Add function tests
+function test_add_known_integer_case
+    a = 2;
+    b = 2;
 
-    methods (TestClassTeardown)
-    end
+    expected_value = 4;
+    actual_value = add(a,b);
 
-    methods (Test)
-        function test_add_known_integer_case(testCase)
-            a = 2;
-            b = 2;
+    assertEqual(actual_value, expected_value);
 
-            expected_value = 4;
-            actual_value = add(a,b);
+function test_add_known_array_case
+    a = [0, 1, 2, 3];
+    b = [4, 5, 6, 7];
 
-            testCase.assertEqual(actual_value, expected_value);
-        end
-        
-        function test_add_known_array_case(testCase)
-            a = [0, 1, 2, 3];
-            b = [4, 5, 6, 7];
+    expected_value = [4, 6, 8, 10];
+    actual_value = add(a,b);
 
-            expected_value = [4, 6, 8, 10];
-            actual_value = add(a,b);
-
-            testCase.assertEqual(actual_value, expected_value);
-        end
-    end
-
-end
+    assertEqual(actual_value, expected_value);
